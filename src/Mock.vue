9a6c1d89d5b18d298812384eaf3057e6b38052e3
hgjinfan <18608664708@163.com>
2018-10-09 00:18:24 +0800

    test

---
+++ src/Mock.vue
@@ -0,0 +1,17 @@
+<template></template>
+<style></style>
+<script>
+import Mock from "mockjs";
+export default {
+  created() {
+    Mock.mock('/hello.json', {
+      "list|1-10": [
+        {
+          "id|+1": 1,
+          email: "@EMAIL"
+        }
+      ]
+    });
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,12 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="connect" @click="connect">connectBtn</div>
+    <div v-text="status"></div>
+    <ul>
+      <li v-for='(log, index) in list' v-text='log.data' :key='index'></li>
+    </ul>
+    <input type="text" v-model='log'> <span @click='sendMessage(log)'>sendBtn</span>
   </div>
 </template>
 
@@ -11,15 +17,61 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "home",
+  data() {
+    return {
+      websoket: null,
+      wsUrl: "wss://echo.websocket.org/",
+      list: [],
+      status: "close",
+      log: ""
+    };
+  },
   components: {
     HelloWorld
   },
+  methods: {
+    connect() {
+      this.websoket = new WebSocket(this.wsUrl);
+      this.status = "connect..."
+      this.websoket.onopen = () => {
+        this.status = "connected"
+      }
+      this.websoket.onclose = () => {
+        this.status = 'close'
+      }
+      this.websoket.onmessage = (e) => {
+        this.list.push(e)
+      }
+    },
+    sendMessage(text) {
+      this.websoket.send(text)
+    }
+  },
   created() {
-    this.$http.get("/hello.json").then(response => {
-      console.log(response)
-    }).catch(erro => {
-      console.error(erro)
-    })
+    this.$http
+      .get("/hello.json")
+      .then(response => {
+        // eslint-disable-next-line
+        console.log(response);
+      })
+      .catch(erro => {
+        // eslint-disable-next-line
+        console.error(erro);
+      });
+    this.$http
+      .post("http://api.xiaojumuying.com/mobile/Index/getStaffList", {
+        type: "月嫂",
+        name: "",
+        age: ["50~60"],
+        workYears: "",
+        education: [],
+        province: "",
+        page: 1
+      })
+      .then(function(response) {
+        // eslint-disable-next-line
+        console.log(response);
+      });
   }
 };
 </script>
